32a1abb8170f275ed5d04303e0278fb85f824e40
dhilipkumarm2008 <dhilipkumar.m_25it@kgkite.ac.in>
2026-08-24 15:17:23 +0530

    palinedromenumber

---
+++ CheckPalindromeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CheckPalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int temp = n;
+        int reverse = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp /= 10;
+        }
+
+        if (n == reverse)
+            System.out.println(n + " is a Palindrome Number");
+        else
+            System.out.println(n + " is not a Palindrome Number");
+    }
+}
