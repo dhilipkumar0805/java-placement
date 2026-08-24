f04867229eca384a4f171eaaefe2613220f085bb
dhilipkumarm2008 <dhilipkumar.m_25it@kgkite.ac.in>
2026-08-24 15:23:08 +0530

    neonnumber

---
+++ CheckNeonNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CheckNeonNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int square = n * n;
+        int sum = 0;
+
+        while (square != 0) {
+            sum += square % 10;
+            square /= 10;
+        }
+
+        if (sum == n)
+            System.out.println(n + " is a Neon Number");
+        else
+            System.out.println(n + " is not a Neon Number");
+    }
+}
